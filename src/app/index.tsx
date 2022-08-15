ade5ab535745d3e642b30cd0b0ec4b9424e24fcf
dontmeway <bbakhadyrov@gmail.com>
2022-08-15 13:51:10 +0500

    init: path aliases, effector ecosystem

---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import './main.css'
+
+export const App = () => {
+  return <h1 className="text-3xl text-blue-500">Chipta ol admin</h1>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import fs from 'fs'
+
+const rootPath = __dirname
+
+const basePath = path.resolve(rootPath, './src')
+const srcDirs = fs
+  .readdirSync(basePath)
+  .filter((name) => fs.lstatSync(path.join(basePath, name)).isDirectory())
+
+const srcAliases = srcDirs.reduce(
+  (acc: any, name: any) => ({
+    ...acc,
+    [`~${name}`]: `${path.resolve(rootPath, 'src')}/${name}`,
+  }),
+  {}
+)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 7000,
+  },
+  resolve: {
+    alias: {
+      ...srcAliases,
+    },
+  },
 })
